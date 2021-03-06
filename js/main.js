fc7f011bebaec5eba16296f7bdb1572d8ef632b1
Rastislav Hepner <rastislav.hepner@gmail.com>
2021-03-06 16:20:09 +0100

    ellipse with face styled

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 p = document.getElementById("testimonial");
+ellipsis = document.getElementById("pictureEllipse");
 dummytext = "Donec ac placerat odio. Donec venenatis nibh et urna bibendum, porttitor accumsan tellus molestie. Fusce tincidunt sollicitudin porta. Donec sagittis, lectus at dictum eleifend, tortor ligula consequat turpis, ac lobortis tortor augue ac dolor. Etiam cursus justo vitae scelerisque blandit. Vivamus id lectus et ex condimentum mollis. Mauris aliquam id nulla sed imperdiet."
 p.innerHTML = dummytext;
+ellipsis.style.backgroundImage = 'url("img/customer-1.jpg")';
