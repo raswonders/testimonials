c789ec297579b973642a66a9ed70436ad4fe63c4
Rastislav Hepner <rastislav.hepner@gmail.com>
2021-03-20 12:41:24 +0100

    Reimplemented testimonial scrolling and handling of click events

--- js/main.js
+++ js/main.js
@@ -1,29 +1,51 @@
-class Customer {
-    constructor(picture, testimonial) {
-        this.picture = picture
-        this.testimonial = testimonial
+(function() {
+    class Customer {
+        constructor(picture, testimonial) {
+            this.picture = picture
+            this.testimonial = testimonial
+        }
     }
-}
 
-customers = []
-customers.push(new Customer('img/customer-1.jpg', 'Ut nisi velit, placerat eu euismod id, vestibulum id neque. Fusce mauris dolor, pharetra dictum laoreet ultricies, ornare vitae elit.'))
-customers.push(new Customer('img/customer-2.jpg', 'Fusce aliquam nibh vitae facilisis suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc ut quam ante. Nulla vitae nisl lobortis arcu porta condimentum. Nam sodales, metus vitae pharetra luctus, lectus lorem dignissim eros, vel.'))
-customers.push(new Customer('img/customer-3.jpg', 'Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum eu ante nec felis lobortis convallis. Sed consequat augue ut elit blandit, a auctor arcu aliquet. Aliquam interdum lacus justo, eget eleifend lacus vulputate.'))
+    const ellipsis = document.getElementById("pictureEllipse");
+    const p = document.getElementById("testimonial");
+    let index = 0
+    const customers = []
+    customers.push(new Customer('img/customer-1.jpg', 'Ut nisi velit, placerat eu euismod id, vestibulum id neque. Fusce mauris dolor, pharetra dictum laoreet ultricies, ornare vitae elit.'))
+    customers.push(new Customer('img/customer-2.jpg', 'Fusce aliquam nibh vitae facilisis suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc ut quam ante. Nulla vitae nisl lobortis arcu porta condimentum. Nam sodales, metus vitae pharetra luctus, lectus lorem dignissim eros, vel.'))
+    customers.push(new Customer('img/customer-3.jpg', 'Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum eu ante nec felis lobortis convallis. Sed consequat augue ut elit blandit, a auctor arcu aliquet. Aliquam interdum lacus justo, eget eleifend lacus vulputate.'))
 
-const p = document.getElementById("testimonial");
-const ellipsis = document.getElementById("pictureEllipse");
-let cu_index = 0
+    const buttons = document.querySelectorAll('.btn');
 
-function displayTestimonial() {
-    let cu = customers[cu_index]
-    ellipsis.style.backgroundImage = `url(${cu.picture})`;
-    p.innerHTML = cu.testimonial;
-}
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            // left button
+            if (e.target.classList.contains('btn-left') ||
+                e.target.id === 'use-left') {
+                index--
+            // right button
+            } else if (e.target.classList.contains('btn-right') ||
+                e.target.id === 'use-right') {
+                index++
+            }
 
-function indexChange(step) {
-    let i = cu_index;
-    i += step
-    i = i % customers.length;
-    if (i < 0) i += customers.length
-    cu_index = i;
-}
+            // If index overflows handle it
+            if ( index < 0 ) {
+                index = customers.length - 1
+            } else if ( index > customers.length ) {
+                index = 0
+            }
+
+            // update page
+            displayTestimonial(index)
+        })
+    })
+
+    function displayTestimonial(index) {
+        console.log(index)
+        let cu = customers[index]
+        ellipsis.style.backgroundImage = `url(${cu.picture})`;
+        p.innerHTML = cu.testimonial;
+    }
+
+    displayTestimonial(index)
+})()
